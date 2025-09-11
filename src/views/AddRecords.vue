9a8508702c20d6e17acad1b59f5447b6e441fae1
Arcgel <darkdusty8000@gmail.com>
2025-09-12 06:16:47 +0800

    feat(records): Add form component for new patient records
    This commit introduces a new Vue component that provides a form for adding new patient medical records. This component allows for the capture of key information, including:
    
    Symptom and Diagnosis descriptions
    
    Patient-specific notes and a description of the condition
    
    Possible interventions or treatments
    
    A checklist for lab work (e.g., CBC, X-ray)
    
    The form uses a addRecordsHandler function to collect the data from the input fields and process the record submission. This component is a critical addition for managing and documenting patient medical history.

---
+++ src/views/AddRecords.vue
@@ -0,0 +1,61 @@
+<template>
+	<div>
+		<form submit.prevent="addRecordHandler">
+
+			<label for="symptom">Symptom: </label>
+			<textarea name="symptom" id="symptom"></textarea>
+
+			<label for="diagnosis">Diagnosis: </label>
+			<textarea name="diagnosis" id="diaganosis"></textarea>
+
+			<label for="desciption">Dscription: </label>
+			<textarea name="desciption" id="desciption"></textarea>
+
+			<div class="labcheck">
+
+				<input type="checkbox" id="nonefornow" name="nonefornow" value="none for now">
+				<label for="nonefornow">None for now</label>
+
+				<input type="checkbox" id="cbc" name="cbc" value="cbc">
+				<label for="cbc">cbc</label>
+
+				<input type="checkbox" id="x-ray" name="x-ray" value="x-ray">
+				<label for="x-ray">x-ray</label>
+			</div>
+
+			<label for="intervention">Possible Intervention: </label>
+			<textarea name="intervention" id="intervention"></textarea>
+
+			<button type="submit">Record</button>
+
+		</form>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import PatientRecords from '@/views/PatientRecords.vue';
+
+const record = PatientRecords()
+
+const symptom = ref('')
+const diagnosis = ref('')
+const desciption = ref('')
+const intervention = ref('')
+
+const addRecordsHandler = () => {
+	record.addRecords({
+		symptom: symptom.value,
+		diagnosis: diagnosis.value,
+		desciption: desciption.value,
+		intervention: intervention.value
+	})
+
+	symptom.value = ''
+	diagnosis.value = ''
+	desciption.value = ''
+	intervention.value = ''
+}
+</script>
+
+<style lang="scss" scoped></style>
