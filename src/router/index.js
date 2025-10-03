1a33c0f0f7e62240a39f35aaf5cbae1c937c54dc
Acrgel <darkdusty8000@gmail.com>
2025-10-03 10:25:21 +0800

    feat: added auth to login ensuring credentials before access
    note: Still broken, will be fixed later

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore'
 import Patientview from '../views/PatientView.vue'
 import PatientRecords from '@/views/PatientRecords.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,13 +11,33 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Patientview,
+      // meta: { requiresAuth: true }
     },
     {
       path: '/patientrecords/:id',
       name: 'patientrecords',
       component: PatientRecords,
+      // meta: { requiresAuth: true }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if(to.meta.requiresAuth) {
+    if(authStore.isAutheticated){
+      next()
+    } else {
+      next('/login')
+    }
+  } else{
+    next()
+  }
 })
 
 export default router
