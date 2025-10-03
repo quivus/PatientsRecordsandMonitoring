df7c63994c33ae0d3662495bbdf53af6b044f6ba
Acrgel <darkdusty8000@gmail.com>
2025-10-03 10:28:37 +0800

    feat:
    Added form for ligin

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,25 @@
 <template>
-  <div></div>
+  <div>
+    <form @submit.prevent="submitHandler">
+      <label for="username">Username: </label>
+      <input type="text" id="username" v-model="auth.formLogin.username" />
+
+      <label for="password">Password: </label>
+      <input type="text" id="password" v-model="auth.formLogin.password" />
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useAuthStore } from '@/stores/authStore'
+
+const auth = useAuthStore()
+
+const submitHandler = () => {
+  auth.login()
+}
+</script>
 
 <style lang="scss" scoped></style>
